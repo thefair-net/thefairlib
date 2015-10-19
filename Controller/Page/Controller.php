bc57d07cd0010900575cc6a783a7a806736b6337
zhanghan <zhanghan@thefair.net.cn>
2015-10-19 17:54:10 +0800

    up

---
+++ Controller/Page/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Controller.php
+ *
+ * @author ZhangHan <zhanghan@thefair.net.cn>
+ * @version 1.0
+ * @copyright 2015-2025 TheFair
+ */
+namespace TheFairLib\Controller\Page;
+
+use TheFairLib\Controller\Base;
+
+class Controller extends Base
+{
+
+}
