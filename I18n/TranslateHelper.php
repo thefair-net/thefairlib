5c2d691e3904c264545052ce2431896d5cc45219
zhanghan <zhanghan@thefair.net.cn>
2015-12-25 14:26:03 +0800

    fix

--- I18n/TranslateHelper.php
+++ I18n/TranslateHelper.php
@@ -42,12 +42,13 @@ class TranslateHelper
      * 翻译
      * 翻译的语言包配置到/app/root/path/config/i18n/$lang/
      *
-     * @param string $type  针对语言包中的目录，比如api/error.php，对于type为api_error
+     * @param string $type 针对语言包中的目录，比如api/error.php，对于type为api_error
      * @param string $label 具体的配置文件中的数值key，参考config使用方法
-     * @return mixed
+     * @param string $lang
+     * @return bool
      */
-    static public function translate($type, $label){
-        $funcName = 'get_i18n_'.self::getLang().'_'.$type;
+    static public function translate($type, $label, $lang = ''){
+        $funcName = 'get_i18n_'.(empty($lang) || !in_array($lang, ['cn', 'jp']) ? self::getLang() : $lang).'_'.$type;
         $ret = Config::$funcName($label);
         return empty($ret) ? false : $ret;
     }
