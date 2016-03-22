64962b2d2dccae87baf78c0291c0a085e6049a4e
zhanghan <zhanghan@thefair.net.cn>
2016-03-22 17:30:47 +0800

    fix

--- Service/Swoole/Client/TCP.php
+++ Service/Swoole/Client/TCP.php
@@ -11,7 +11,7 @@ class TCP extends Base
             exit("connect failed. Error: {$client->errCode}\n");
         }
         $client->send($data);
-        $result = $client->recv(655350);
+        $result = $client->recv(105535);
         $client->close();
 
         return $result;
