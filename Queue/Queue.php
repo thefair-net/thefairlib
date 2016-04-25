24f7de0ed10c0a30956b1738325199e5def27925
liumingzhij26 <liumingzhi@thefair.net.cn>
2016-04-25 11:00:00 +0800

    fix

---
+++ Queue/Queue.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Base.php
+ *
+ * @author liumingzhi
+ * @version 1.0
+ * @copyright 2015-2015
+ * @date 16/4/14 下午2:23
+ */
+
+namespace TheFairLib\Queue;
+
+use TheFairLib\Queue\Kafka\Kafka;
+
+class Queue
+{
+    static public $instance;
+
+    private $_server = 'kafka';
+
+    /**
+     * @return Queue
+     */
+    static public function Instance()
+    {
+        $class = get_called_class();
+        if (empty(self::$instance)) {
+            self::$instance = new $class();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * @return Kafka
+     * @throws \Exception
+     */
+    public function getApplication()
+    {
+        switch ($this->_server) {
+            case 'kafka' :
+                return Kafka::Instance();
+                break;
+        }
+        throw new \Exception('error ', $this->_server);
+    }
+}
