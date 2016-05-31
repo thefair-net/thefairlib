f4a53224dd1b0e7f0a8e5f61ef8f67732c934ca9
zhanghan <zhanghan@thefair.net.cn>
2016-05-31 11:27:50 +0800

    fix

---
+++ Model/DataModel.php
@@ -0,0 +1,297 @@
+<?php
+/**
+ * DataModel.php
+ *
+ * @author ZhangHan <zhanghan@thefair.net.cn>
+ * @version 1.0
+ * @copyright 2015-2025 TheFair
+ */
+namespace TheFairLib\Model;
+
+use Illuminate\Database\Capsule\Manager;
+use TheFairLib\Config\Config;
+use TheFairLib\DB\Redis\Cache;
+use TheFairLib\DB\Redis\Storage;
+use TheFairLib\Exception\Api\ApiException;
+use TheFairLib\Utility\Utility;
+
+abstract class DataModel
+{
+
+    protected $server = 'default';
+
+    static protected $instance;
+
+    static protected $cache;
+
+    static protected $db = [];
+
+    static protected $dbName = 'default';
+
+    protected static $_params;
+
+    /**
+     * 不含前缀和sharding后缀的表名
+     * 例如taoo_user_0:
+     * $_tableName = 'user';
+     *
+     * @var string
+     */
+    protected $_tableName = null;
+
+    protected $_shardingKey = null;
+
+    protected $_shardingNum = null;
+
+    /**
+     * @var Manager
+     */
+    static protected $capsule;
+
+    static public function Instance()
+    {
+        $class = get_called_class();
+        if (empty(self::$instance[$class])) {
+            self::$instance[$class] = new $class();
+        }
+        self::$_params = Utility::get_api_params();
+        return self::$instance[$class];
+    }
+
+
+    /**
+     * 存储
+     * @return \Redis
+     */
+    protected function Storage()
+    {
+        return Storage::getInstance($this->server);
+    }
+
+    /**
+     * 缓存
+     * @return \Redis
+     */
+    protected function Cache()
+    {
+        return Cache::getInstance($this->server);
+    }
+
+    /**
+     * @param $type
+     * @param $dataType
+     * @return string
+     * @throws Exception
+     */
+    protected function getPrefix($type, $dataType)
+    {
+        if (!in_array($type, ['Cache', 'Storage']) || !in_array($dataType, ['key', 'hash', 'set', 'zset', 'list', 'string'])) {
+            throw new Exception('Redis cahce prefix config error!');
+        }
+        return $type . '#' . Config::get_app('phase') . '#' . $dataType . '#';
+    }
+
+    /**
+     * @param $dbName
+     * @return \Illuminate\Database\Connection
+     * @throws ApiException
+     */
+    protected function db($dbName = '')
+    {
+        if (empty($dbName)) {
+            $dbName = static::$dbName;
+        }
+
+        if (empty(self::$capsule)) {
+            self::$capsule = new Manager();
+            self::$capsule->setAsGlobal();
+        }
+
+        if (!in_array($dbName, self::$db)) {
+            $conf = Config::get_db_mysql($dbName);
+            if (empty($conf)) {
+                throw new ApiException('sys_db:' . $dbName);
+            }
+
+            self::$capsule->addConnection($conf, $dbName);
+            self::$capsule->setAsGlobal();
+            self::$db[] = $dbName;
+        }
+        $tmp = self::$capsule;
+        return $tmp::connection($dbName);
+
+
+    }
+
+    /**
+     * 获取数据库uuid
+     *
+     * @return int uuid
+     * @throws ApiException
+     */
+    protected function _getUuid()
+    {
+        $ret = $this->db('default')->select('select uuid_short() as uuid');
+        $uuid = $ret[0]['uuid'];
+        if (!empty($uuid)) {
+            return $uuid;
+        } else {
+            throw new ApiException('uuid error', [], 50000);
+        }
+    }
+
+    protected function _getTableName($shardingKey = null, $tableName = '')
+    {
+        $tableName = !empty($tableName) ? $tableName : $this->_tableName;
+        if (empty($tableName) || ($shardingKey !== null && empty($this->_shardingNum))) {
+            throw new ApiException('M Conf Err');
+        }
+        return $this->_tableName . ($shardingKey !== null ? '_' . $this->_getShardingTableNum($shardingKey) : '');
+    }
+
+    private function _getShardingTableNum($shardingKey)
+    {
+        return (int)$shardingKey % $this->_shardingNum;
+    }
+
+    /**
+     * 分页获取数据的方法
+     *
+     * @param string $dbName 数据库名称
+     * @param string $tableName 表名
+     * @param string|array $where where子句
+     * @param array $fields 需要查询的数据库字段数组
+     * @param int $page 当前请求页数
+     * @param string $order 排序规则，如：'uid desc'
+     * @param int $itemPerPage 每页返回多少行数据
+     * @return array 结果集
+     * @throws ApiException
+     */
+    protected function _getItemListByPage($dbName, $tableName, $where, $fields, $page = 1, $order = '', $itemPerPage = 20)
+    {
+        $shardingKey = $this->_shardingKey === null ? '*' : $this->_shardingKey;
+        $sqlObj = $this->db($dbName)->table($tableName);
+        if (!empty($where)) {
+            $sqlObj = is_array($where) && count($where) == 2 ? $sqlObj->whereRaw($where[0], $where[1]) : $sqlObj->whereRaw($where);
+        }
+        $total = $sqlObj->count($shardingKey);
+        $itemPerPage = min(50, $itemPerPage);
+        $pageCount = ceil($total / $itemPerPage);
+        if (!empty($total)) {
+            $sqlObj = $this->db($dbName)->table($tableName);
+            if (!empty($where)) {
+                $sqlObj = is_array($where) && count($where) == 2 ? $sqlObj->whereRaw($where[0], $where[1]) : $sqlObj->whereRaw($where);
+            }
+            if (!empty($order)) {
+                $sqlObj = $sqlObj->orderByRaw($order);
+            }
+            $ret = $sqlObj->limit($itemPerPage)->offset(($page - 1) * $itemPerPage)->get($fields);
+        } else {
+            $ret = [];
+        }
+
+        return [
+            'item_list' => $ret,
+            'item_count' => $total,
+            'item_per_page' => $itemPerPage,
+            'page' => $page,
+            'page_count' => $pageCount,
+            'last_item_id' => 0,
+        ];
+    }
+
+    /**
+     * 获取单条数据
+     *
+     * @param $dbName
+     * @param $tableName
+     * @param $where
+     * @param $fields
+     * @return mixed|static
+     * @throws ApiException
+     */
+    protected function _getItem($dbName, $tableName, $where, $fields = ['*'])
+    {
+        return $this->db($dbName)->table($tableName)->whereRaw($where)->first($fields);
+    }
+
+    /**
+     * 生成唯一标识
+     *
+     * @param string $tag
+     * @return string
+     */
+    public function getUniqid($tag = 'tag')
+    {
+        return md5(uniqid($tag, true));
+    }
+
+    /**
+     * @param $shardingKeys
+     * @param string $order
+     * @param array $fields
+     * @param string $extWhere
+     * @param bool $useShardingKeyMerge
+     * @return array
+     * @throws ApiException
+     */
+    public function batchGetByShardingKeys($shardingKeys, $order = '', $fields = ['*'], $extWhere = '', $useShardingKeyMerge = true)
+    {
+        $ret = [];
+        $search = [];
+        if ($fields != ['*'] && !in_array($this->_shardingKey, $fields)) {
+            $fields[] = $this->_shardingKey;
+        }
+        if (!empty($shardingKeys)) {
+            foreach ($shardingKeys as $key) {
+                $search[$this->_getTableName($key)][] = $key;
+            }
+
+            foreach ($search as $tableName => $keys) {
+                $where = "{$this->_shardingKey} in ('" . implode("','", $keys) . "')" . (!empty($extWhere) ? ' and ' . $extWhere : '');
+                $tmpSqlObj = $this->db(static::$dbName)->table($tableName)->whereRaw($where);
+                if (!empty($order)) {
+                    $tmpSqlObj = $tmpSqlObj->orderByRaw($order);
+                }
+                $tmpRet = $tmpSqlObj->get($fields);
+                if (!empty($tmpRet)) {
+                    if ($useShardingKeyMerge === true) {
+                        foreach ($tmpRet as $tmpItem) {
+                            $ret[$tmpItem[$this->_shardingKey]] = $tmpItem;
+                        }
+                    } else {
+                        $ret = array_merge($ret, $tmpRet);
+                    }
+                }
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * 设置当次会话静态缓存数据
+     *
+     * @param $prefix
+     * @param $key
+     * @param array $data
+     */
+    public function setSessionCache($prefix, $key, $data = [])
+    {
+        $set_keys = "set_{$prefix}_{$key}";
+        Utility::$set_keys($data);
+    }
+
+    /**
+     * 获取当次会话缓存数据
+     *
+     * @param $prefix
+     * @param $key
+     * @return mixed
+     */
+    public function getSessionCache($prefix, $key)
+    {
+        $get_keys = "get_{$prefix}_{$key}";
+        return Utility::$get_keys();
+    }
+}
--- Utility/Utility.php
+++ Utility/Utility.php
@@ -430,4 +430,32 @@ class Utility
         $replace = array("", "", "", "", "");
         return str_replace($search, $replace, $str);
     }
+
+    /**
+     * 用于随机抽奖
+     * 例如:
+     * $check = Utility::getRandomLottery(['Y' => 6, 'N' => 4]);
+     * $check的值为Y/N,$proArr为随机几率
+     *
+     * @param $proArr
+     * @return int|string
+     */
+    public static function getRandomLottery($proArr)
+    {
+        $result = '';
+        //概率数组的总概率精度
+        $proSum = array_sum($proArr);
+        //概率数组循环
+        foreach ($proArr as $key => $proCur) {
+            $randNum = mt_rand(1, $proSum);             //抽取随机数
+            if ($randNum <= $proCur) {
+                $result = $key;                         //得出结果
+                break;
+            } else {
+                $proSum -= $proCur;
+            }
+        }
+        unset ($proArr);
+        return $result;
+    }
 }
