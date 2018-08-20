1b55ecba27fd13b7c8c64bc8c51a4380255f3121
liumingzhi <liumingzhij26@qq.com>
2018-08-20 14:13:45 +0800

    fix: 关闭连接

--- Queue/Rabbitmq/Rabbitmq.php
+++ Queue/Rabbitmq/Rabbitmq.php
@@ -59,8 +59,10 @@ class Rabbitmq
     static public function closeConnection()
     {
         if (!empty(self::$instance) && !empty(self::$_conn)) {
-            self::$_channel->close();
-            self::$_conn->close();
+            if(self::$_conn->isConnected()) {
+                self::$_channel->close();
+                self::$_conn->close();
+            }
             self::$_conn = null;
             self::$_channel = null;
             self::$instance = null;
