190ca8b359b6bee05bd157550ec91634c102a118
lijingyu <lijingyu@thefair.net.cn>
2019-03-14 14:17:03 +0800

    kafka v0.1 => v0.2

--- Logger/KafkaLogger.php
+++ Logger/KafkaLogger.php
@@ -8,12 +8,15 @@
 
 namespace TheFairLib\Logger;
 
-use Kafka\Produce;
-use TheFairLib\Queue\Inter;
+use Kafka\Producer;
+use Kafka\ProducerConfig;
+use TheFairLib\Config\Config;
+use TheFairLib\Utility\Utility;
 
 
 class KafkaLogger
 {
+
     private static $instance = null;
 
     const KAFKA_NAME_START = 'log';
@@ -30,80 +33,32 @@ class KafkaLogger
         return self::$instance;
     }
 
-
     /**
-     * 获取路由地址
+     * 发送普通队列消息.
      *
-     * @param $route_name
-     * @return string
+     * @param $topicName
+     * @param $message
+     * @param string $groupName
      */
-    protected function getRouteAction($route_name)
+    public function sendDirectMessage($topicName, $message, $groupName = self::KAFKA_NAME_START)
     {
-        $route = [];
-        $config = \TheFairLib\Config\Config::get_api_kakfa();
-        if (empty($config['inner_domain'])) {
-            return fasle;
-        }
-        $route[] = rtrim($config['inner_domain'],'/');
-        $route[] = self::KAFKA_NAME_START;
-        $route[] = $route_name;
-        return implode('/',$route).'/';
+        $kafkaConf = Config::get_queue_kafka($groupName);
+
+        $config = ProducerConfig::getInstance();
+        $config->setMetadataRefreshIntervalMs(10000);
+        $config->setMetadataBrokerList($kafkaConf['host']);
+        $config->setRequiredAck(0);
+        $config->setIsAsyn(false);
+        $config->setProduceInterval(500);
+        $producer = new Producer();
+        $producer->send(array(
+            array(
+                'topic' => $topicName,
+                'value' => $message,
+                'key' => '',
+            )
+        ));
     }
-
-
-    /**
-     * 发送DB队列代理消息
-     *
-     * @param $database
-     * @param $table
-     * @param $primary
-     * @return bool
-     */
-    public function proxyDbMessage($database,$table,$primary)
-    {
-        if (empty($database) || empty($table) || empty($primary) || !is_array($primary)) {
-            return false;
-        }
-        $message['database'] = $database;
-        $message['table']    = $table;
-        $message['primary']  = \TheFairLib\Utility\Utility::encode($primary);
-        return $this->sendProxyMessage($database,$message);
-    }
-
-
-    /**
-     * 发送代理队列消息
-     *
-     * @param string $route_name
-     * @param array $message
-     */
-    public function sendProxyMessage($route_name,$message)
-    {
-        $request_action = $this->getRouteAction($route_name);
-        if (empty($request_action)) {
-            return false;
-        }
-        return \TheFairLib\Http\Curl()->post($request_action,$message);
-    }
-
-
-    /**
-     * 发送普通队列消息
-     *
-     * @param string $topicName
-     * @param array  $message
-     * @return bool
-     */
-    public function sendDerectMessage($topicName,$message,$group_name=self::KAFKA_NAME_START)
-    {
-        $config = \TheFairLib\Config\Config::get_queue_kafka($group_name);
-        $produce = Produce::getInstance($config['zookeeper'],$config['timeout'],$config['host']);
-        $produce->getAvailablePartitions($topicName);
-        $produce->setRequireAck(-1);
-        $produce->setMessages($topicName,0,$message);
-        return $produce->send();
-    }
-
 
     /**
      * 发送DB直接消息
@@ -111,17 +66,17 @@ class KafkaLogger
      * @param $database
      * @param $table
      * @param $primary
-     * @return bool
      */
-    public function derectDbMessage($database,$table,$primary)
+    public function directDbMessage($database, $table, $primary)
     {
         if (empty($database) || empty($table) || empty($primary) || !is_array($primary)) {
-            return false;
+            return;
         }
         $message[] = $database;
         $message[] = $table;
-        $message[] = \TheFairLib\Utility\Utility::encode($primary);
-        $topicName = self::KAFKA_NAME_START.implode('',array_map('ucfirst',explode('_',$database)));
-        return $this->sendDerectMessage($topicName,$message);
+        $message[] = Utility::encode($primary);
+        $topicName = self::KAFKA_NAME_START . implode('', array_map('ucfirst', explode('_', $database)));
+        $this->sendDirectMessage($topicName, $message);
     }
+
 }
