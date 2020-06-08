ae88ca5f39e33fcb6ca59000dd8ce337e0bc3cb0
mingzhi.L <liumingzhij26@gmail.com>
2020-06-08 18:52:43 +0800

    feat:  smart

--- src/Service/JsonRpc/RpcClient/Client.php
+++ src/Service/JsonRpc/RpcClient/Client.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace TheFairLib\Service\JsonRpc\RpcClient;
 
+use TheFairLib\Config\Config;
+use TheFairLib\DB\Redis\Cache;
 use TheFairLib\Exception\Service\ServiceException;
 use TheFairLib\Service\Swoole\Client\TCP;
 use TheFairLib\Service\JsonRpc\DataFormatter;
 use TheFairLib\Service\JsonRpc\JsonLengthPacker;
+use TheFairLib\Utility\Utility;
 use Throwable;
+use Yaf\Exception;
 
 class Client extends TCP
 {
@@ -64,8 +68,73 @@ class Client extends TCP
         }
     }
 
+    /**
+     * 智能获取数据
+     *
+     * @param $url
+     * @param array $params
+     * @param bool $showResultOnly
+     * @return bool|mixed|string
+     * @throws Exception
+     * @throws \TheFairLib\Service\Exception
+     */
+    public function smart($url, $params = [], $showResultOnly = true)
+    {
+        $cacheTtl = $this->_getServiceCacheTtl($url);
+        //获取缓存的key
+        $cacheKey = $this->_getServiceCacheKey($url, $params);
+        if ($cacheTtl !== false) {
+            $result = $this->_getCache()->get($cacheKey);
+        }
+
+        if (empty($result)) {
+            $result = $this->call($url, $params);
+            if ($cacheTtl !== false) {
+                $this->_getCache()->setex($cacheKey, $cacheTtl, Utility::encode($result));
+            }
+        } else {
+            $result = Utility::decode($result);
+        }
+
+        //如果设置了只返回结果,当code!=0的时候,直接抛出异常
+        if ($showResultOnly === true) {
+            if (!empty($result['code'])) {
+                throw new \TheFairLib\Service\Exception($result['message'], $result['code']);
+            } else {
+                return $result['result'];
+            }
+        } else {
+            return $result;
+        }
+    }
+
     protected function _getClientType()
     {
         return 'rpc';
+    }
+
+    private function _getServiceCacheTtl($url)
+    {
+        $key = $this->_getClientType() . '.' . $this->getServerTag() . '.' . str_replace('/', '_', substr($url, 1));
+        $ttl = Config::get_service_cache($key);
+        return empty($ttl) ? false : $ttl;
+    }
+
+    private function _getServiceCacheKey($url, $params)
+    {
+        $serviceConfig = $this->_getServiceConfigKey($url);
+        return !empty($serviceConfig) ? 'service_cache_' . Config::get_app('phase') . '::' . $serviceConfig . '_' . md5($this->getServerTag() . $url . Utility::encode($params)) : null;
+    }
+
+    private function _getServiceConfigKey($url)
+    {
+        return strtolower($this->getServerTag() . '::' . str_replace('/', '_', substr($url, 1)));
+    }
+
+    private function _getCache()
+    {
+        $serviceConf = $this->_getServiceConfig($this->getServerTag());
+        $cacheNode = !empty($serviceConf['cache_node']) ? $serviceConf['cache_node'] : 'default';
+        return Cache::getInstance($cacheNode);
     }
 }
