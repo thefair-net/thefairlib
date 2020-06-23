48bfd1a5a8c63c067ed5f10392cb0400121b3f2d
mingzhi.L <liumingzhij26@gmail.com>
2020-06-23 10:54:34 +0800

    feat:  日志优化处理

--- src/Listener/ErrorHandleListener.php
+++ src/Listener/ErrorHandleListener.php
@@ -15,8 +15,8 @@ namespace TheFairLib\Listener;
 use ErrorException;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BootApplication;
+use Hyperf\HttpMessage\Server\Response;
 use Hyperf\HttpMessage\Stream\SwooleStream;
-use Hyperf\Utils\Coroutine;
 use TheFairLib\Constants\ServerCode;
 use TheFairLib\Library\Logger\Logger;
 use Psr\Http\Message\ResponseInterface;
@@ -37,60 +37,52 @@ class ErrorHandleListener implements ListenerInterface
     public function process(object $event)
     {
         set_error_handler(function ($level, $message, $file = '', $line = 0, $context = []) {
-            $errors = [
-                'level' => $level,
-                'message' => $message,
-                'file' => $file,
-                'line' => $line,
-                'args' => $_SERVER['argv'],
-            ];
             if (!preg_match('/cli_set_process_title/', $message)) {
                 Logger::get()->debug("mac: " . $message);
             }
             if (error_reporting() & $level) {
-                Logger::get()->error(encode($errors));
+                Logger::get()->error(sprintf('set_error_handler:%s', $message), [
+                    'level' => $level,
+                    'msg' => $message,
+                    'file' => $file,
+                    'line' => $line,
+                    'content' => $context,
+                ]);
                 throw new ErrorException($message, 0, $level, $file, $line);
             }
         });
 
         set_exception_handler(function (Throwable $e) {
-            $errors = [
-                'message' => $e->getMessage(),
+            Logger::get()->error(sprintf('set_exception_handler:%s', $e->getMessage()), [
+                'msg' => $e->getMessage(),
                 'file' => $e->getFile(),
                 'line' => $e->getLine(),
-                'string' => $e->getTraceAsString(),
-                'args' => implode('::', $_SERVER['argv']),
-            ];
-            rd_debug([$errors, __CLASS__, posix_getpid()]);
-
-            Logger::get()->error(encode($errors));
+                'trace_string' => $e->getTraceAsString(),
+            ]);
             throw $e;
         });
+
         register_shutdown_function(function () {
             $error = error_get_last();
             if ($error === null) {
                 return;
             }
-            rd_debug([$error, __CLASS__, posix_getpid()]);
             switch ($error['type'] ?? null) {
                 case E_ERROR:
                 case E_PARSE:
                 case E_CORE_ERROR:
                 case E_COMPILE_ERROR:
-                    // log or send:
-                    // error_log($message);get
-                    // $server->send($fd, $error['message']);
+                    $msg = arrayGet($error, 'message');
+                    Logger::get()->error(sprintf('register_shutdown_function:%s', $msg), [
+                        'msg' => $msg,
+                        'file' => arrayGet($error, 'file'),
+                        'line' => arrayGet($error, 'line'),
+                    ]);
                     /**
                      * @var ResponseInterface $response
                      */
-                    $response = new \Hyperf\HttpMessage\Server\Response();
-                    $error['debug'] = [
-                        'pid' => posix_getpid(),
-                        'cid' => Coroutine::id(),
-                        'c_pid' => Coroutine::parentId(),
-                    ];
-                    Logger::get()->error(encode($error));
-                    $response->withStatus(ServerCode::SERVER_ERROR)->withBody(new SwooleStream($error['message']))->send(true);
+                    $response = new Response();
+                    $response->withStatus(ServerCode::SERVER_ERROR)->withBody(new SwooleStream($msg))->send(true);
                     break;
             }
         });
