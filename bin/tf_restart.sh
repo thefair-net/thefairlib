9a6364024d7a65e2d2c987105dad26dc7bee6936
liumingzhi <liumingzhij26@qq.com>
2021-07-22 17:01:52 +0800

    feat: 发布

--- bin/tf_restart.sh
+++ bin/tf_restart.sh
@@ -4,18 +4,12 @@ basepath=$(cd `dirname $0`; pwd)
 cd $basepath
 cd ../../
 
-signal=$1
-
-if [ "${signal}" = "" ];then
-    signal=15
-fi
-
 if [ -f "./runtime/hyperf.pid" ]; then
     pid=$(cat ./runtime/hyperf.pid)
     echo $pid
-    if [ $signal -eq 9 ]; then
-         /bin/kill -9 $pid
-    else
-        /bin/kill -TERM $pid
-    fi
+    /bin/kill -TERM $pid
+    sleep 4
 fi
+
+/usr/bin/php ./bin/hyperf.php manage:start
+nohup /usr/bin/php ./bin/hyperf.php start > ./runtime/info.log 2>&1 &
