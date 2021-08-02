4951a8a31965a034efbdf17d5773b24dc01cb51d
liumingzhi <liumingzhij26@qq.com>
2021-08-02 18:32:27 +0800

    feat: cms label

---
+++ src/Server/Client/CmsService.php
@@ -0,0 +1,19 @@
+<?php
+/***************************************************************************
+ *
+ * Copyright (c) 2020 liumingzhi, Inc. All Rights Reserved
+ *
+ **************************************************************************
+ *
+ * @file Test.php
+ * @author liumingzhi(liumingzhij26@gmail.com)
+ * @date 2020-06-05 15:51:00
+ *
+ **/
+
+namespace TheFairLib\Server\Client;
+
+class CmsService extends JsonRpcClient
+{
+    protected $serviceName = 'cms_service';
+}
