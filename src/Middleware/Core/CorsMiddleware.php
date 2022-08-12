e89eaa46898ae7e96c5af2298126e8f89f214a31
liumingzhi <liumingzhij26@qq.com>
2022-08-12 18:05:28 +0800

    feat: use Hyperf\Utils\Context;

--- src/Middleware/Core/CorsMiddleware.php
+++ src/Middleware/Core/CorsMiddleware.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
 
 namespace TheFairLib\Middleware\Core;
 
-use Hyperf\Context\Context;
+use Hyperf\Utils\Context;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
